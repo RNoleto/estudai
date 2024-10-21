0bb004ee86538bd9d53ccbb1559b3e3e7983a4db
Ramon Noleto <devnoleto@gmail.com>
2024-10-21 18:29:25 -0300

    :tada: Button
    Ajuste de arredondamento de borda

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,7 @@ import Button from './components/ui/Button.vue';
 </script>
 
 <template>
-  <div class="w-[600px] m-auto">
-    <ul>
-      <li>Tailwind Instalado</li>
-    </ul>
+  <div class="w-[400px] m-auto p-4 rounded-md bg-blue-500">
       <Input label="Nome"/>
       <Input label="Email" type="email" />
       <Input label="Senha" type="password"/>
--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -4,16 +4,16 @@ import { tv } from 'tailwind-variants';
 
 // Definindo os estilos do botão
 const button = tv({
-    base: 'flex items-center justify-center gap-2 rounded-lg text-sm font-medium mt-2 shadow-sm shadow-zinc-200',
+    base: 'flex items-center justify-center gap-2 rounded-md text-sm font-medium mt-2 shadow-sm shadow-zinc-200',
     variants: {
         variant: {
-            primary: 'bg-blue-500 text-blue-50 hover:bg-blue-600 ring-blue-500',
+            primary: 'bg-black text-zinc-50 hover:bg-zinc-600 ring-zinc-500',
             secondary: 'bg-zinc-900 text-zinc-50 hover:bg-zinc-800 ring-zinc-900',
             delete: 'bg-red-500 text-red-50 hover:bg-red-600 ring-red-500',
         },
         size: {
             default: 'px-4 py-2.5',
-            full: 'w-full py-1.5',
+            full: 'w-full py-2',
             sm: 'px-3 py-1.5',
             xs: 'px-1.5 py-1',
         },
