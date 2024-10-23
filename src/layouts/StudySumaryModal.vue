974b667c7e169265b0b0e2c2ee115295e7b722f9
Ramon Noleto <devnoleto@gmail.com>
2024-10-23 18:52:40 -0300

    :book: StudySummaryModal Criação do componente Search reutilizavel e página Subjects
    Ajuste do StudySummaryModal para usar o useCurrentDate e pegar a data atual do usuário

--- src/layouts/StudySumaryModal.vue
+++ src/layouts/StudySumaryModal.vue
@@ -1,50 +1,53 @@
-<template>
-    <div v-if="isOpen" class="fixed inset-0 flex items-center justify-center z-50">
-      <div class="bg-white p-6 rounded-lg shadow-lg w-96">
-        <h3 class="text-lg font-bold mb-4">Resumo do Estudo</h3>
-        <p>Tempo Total: {{ totalStudyTime }}</p>
-        <p>Total de Pausas: {{ totalPauses }}</p>
-        <p>Data: {{ formattedDate }}</p>
-  
-        <div class="flex justify-end mt-4">
-          <button @click="close" class="bg-gray-300 text-gray-700 px-4 py-2 rounded">Fechar</button>
-        </div>
-      </div>
-    </div>
-  </template>
-  
-  <script setup>
-  import { defineProps, defineEmits } from 'vue';
-  
-  const props = defineProps({
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import { useCurrentDate } from '../composables/useCurrentDate';
+
+const props = defineProps({
     isOpen: {
-      type: Boolean,
-      required: true,
+        type: Boolean,
+        required: true,
     },
     totalStudyTime: {
-      type: String,
-      default: '00:00',
+        type: String,
+        default: '00:00',
     },
     totalPauses: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
     formattedDate: {
-      type: String,
-      default: new Date().toLocaleDateString(),
+        type: String,
+        default: new Date().toLocaleDateString(),
     },
-  });
-  
-  // Define a função para emitir o evento de fechamento
-  const emit = defineEmits(['onClose']);
-  
-  const close = () => {
+});
+
+// Usando a composable para obter a data formatada
+const { formattedDate } = useCurrentDate();
+
+// Define a função para emitir o evento de fechamento
+const emit = defineEmits(['onClose']);
+
+const close = () => {
     emit('onClose'); // Emite o evento 'onClose' para o componente pai
-  };
-  </script>
-  
-  <style scoped>
-  .fixed {
+};
+</script>
+<template>
+    <div v-if="isOpen" class="fixed inset-0 flex items-center justify-center z-50">
+        <div class="bg-white p-6 rounded-lg shadow-lg w-96">
+            <h3 class="text-lg font-bold mb-4">Resumo do Estudo</h3>
+            <p>Tempo Total: {{ totalStudyTime }}</p>
+            <p>Total de Pausas: {{ totalPauses }}</p>
+            <p>Data: {{ formattedDate }}</p>
+
+            <div class="flex justify-end mt-4">
+                <button @click="close" class="bg-gray-300 text-gray-700 px-4 py-2 rounded">Fechar</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.fixed {
     position: fixed;
-  }
-  </style>
+}
+</style>
