b2dcd022e3d4e3f253b29a377d5464099b39b0ae
Ramon Noleto <ramon.zap@hotmail.com>
2024-10-22 22:48:47 -0300

    :floppy_disk: StudyCicle Criação do componente Search reutilizavel e página Subjects
    Criação da página de Ciclo de Estudos

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
 import LoginRegister from './components/LoginRegister.vue';
 import CarrerSelection from './components/CarrerSelection.vue';
 import Subjects from './components/Subjects.vue';
+import StudyCycle from './components/StudyCycle.vue';
 
 </script>
 
 <template>
   <div class="flex items-center justify-center min-h-screen bg-gray-100">
-     <Subjects/>
+    <div class="max-w-[1200px] bg-zinc-300 p-[60px] shadow-sm shadow-zinc-200 rounded-md">
+      <StudyCycle/>
+    </div>
   </div>
 
 </template>
---
+++ src/components/StudyCycle.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Button from './ui/Button.vue';
+
+</script>
+
+<template>
+    <h2 class="mb-4 font-bold text-xl">Tela de Ciclo de Estudo</h2>
+    <Button class="float-right">Avançar</Button>
+</template>
