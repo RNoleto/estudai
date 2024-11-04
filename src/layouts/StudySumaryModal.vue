65ed9116b69c75e57cdcc928cede0af00060123c
Ramon Noleto <devnoleto@gmail.com>
2024-11-04 16:39:52 -0300

    :book: Ajuste
    Ajuste temporario de layout do modal StudySumaryModal

--- src/layouts/StudySumaryModal.vue
+++ src/layouts/StudySumaryModal.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Button from '../components/ui/Button.vue';
 import { computed } from 'vue';
 import { useTimerStore } from '../stores/useTimerStore';
 
@@ -18,54 +19,23 @@ const closeModal = () => {
 </script>
 
 <template>
-  <div v-if="isOpen" class="modal-overlay">
-    <div class="modal-content">
-      <h2 class="text-lg font-bold mb-4">Resumo de Estudo</h2>
-      
+  <div v-if="isOpen" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+    <div class="bg-white p-5 rounded-lg shadow-lg max-w-sm w-full">
+      <h2 class="text-lg font-bold mb-4 uppercase ">Resumo de Estudo</h2>
       <!-- Exibindo o tempo total de estudo -->
       <div>
         <strong>Tempo Total de Estudo:</strong> {{ totalStudyTime }}
       </div>
-
       <!-- Exibindo o número de pausas -->
       <div>
         <strong>Total de Pausas:</strong> {{ totalPauses }}
       </div>
-      <button @click="$emit('onClose')" class="btn mt-4">Fechar</button>
+      <Button 
+        variant="primary"
+        @click="$emit('onClose')"
+        >
+        Salvar
+      </Button>
     </div>
   </div>
 </template>
-
-<style scoped>
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.modal-content {
-  background: white;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-}
-
-.btn {
-  background-color: #007bff;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-.btn:hover {
-  background-color: #0056b3;
-}
-</style>
