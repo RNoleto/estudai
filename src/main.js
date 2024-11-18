b07fec9ae9580df9d9515823c704b692ce68eb92
Ramon Noleto <devnoleto@gmail.com>
2024-11-18 12:30:16 -0300

    :wrench: chore: Instalação do Vuetify components

--- src/main.js
+++ src/main.js
@@ -3,10 +3,21 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const pinia = createPinia();
 
 const app = createApp(App);
 
+app.use(vuetify)
 app.use(pinia)
 
 app.mount('#app');
