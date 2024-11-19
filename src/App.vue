0b463dc0fbb793f5c9dc6f82289c583b479dbca7
Ramon Noleto <devnoleto@gmail.com>
2024-11-19 16:38:35 -0300

    :wrench: bkp: BKP

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup>
-import StudyStart from './views/StudyStart.vue';
 import LoginRegister from './views/LoginRegister.vue';
 import CareerSelection from './views/CareerSelection.vue';
 import Subjects from './views/Subjects.vue';
 import StudyCycle from './views/StudyCycle.vue';
+import StudyStart from './views/StudyStart.vue';
 
 </script>
 
 <template>
   <div class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="max-w-[1200px] bg-zinc-300 p-[60px] shadow-sm shadow-zinc-200 rounded-md">
-      <Subjects/>
+      <StudyStart/>
     </div>
   </div>
 </template>
