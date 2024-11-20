ee606fa9b0e4ce970a8e75ce6b62cff8ece0aee0
Ramon Noleto <ramon.zap@hotmail.com>
2024-11-19 22:18:50 -0300

    :wrench: feat: Home page base

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { SignedIn, SignedOut, SignInButton, UserButton } from 'vue-clerk'
+
+</script>
+
+<template>
+  <div class="flex w-full h-screen justify-center align-center">
+    <div class="w-full bg-zinc-300 h-full">
+      <!-- Lado Esquerdo -->
+       <div>
+         <SignedOut>
+           <SignInButton />
+         </SignedOut>
+         <SignedIn>
+           <UserButton />
+         </SignedIn>
+       </div>
+    </div>
+    <div class="bg-zinc-500 w-full h-full">
+      <!-- Lado Direito -->
+       <p>Lado que vai ficar a imagem</p>
+    </div>
+  </div>
+</template> 
