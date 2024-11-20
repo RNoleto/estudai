b094aba7ce63a64a7137acb3c1a9491188d3b30f
Ramon Noleto <ramon.zap@hotmail.com>
2024-11-19 21:22:10 -0300

    :wrench: feat: Clerk install home page

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,22 @@ import Subjects from './views/Subjects.vue';
 import StudyCycle from './views/StudyCycle.vue';
 import StudyStart from './views/StudyStart.vue';
 
+import { SignedIn, SignedOut, SignInButton, UserButton } from 'vue-clerk'
+
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+  <!-- <div class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="max-w-[1200px] bg-zinc-300 p-[60px] shadow-sm shadow-zinc-200 rounded-md">
       <StudyStart/>
     </div>
+  </div> -->
+  <div class="w-full">
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { clerkPlugin } from 'vue-clerk'
 import './style.css'
 import App from './App.vue'
 import axios from 'axios'
@@ -14,11 +15,16 @@ const vuetify = createVuetify({
     directives,
 })
 
+
 const pinia = createPinia();
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
 
 const app = createApp(App);
+
+app.use(clerkPlugin, {
+    publishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+});
 
 app.use(vuetify)
 app.use(pinia)
