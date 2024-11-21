0f0ae17590330fcfa80bdf1032ed2e56a1fd3a18
Ramon Noleto <devnoleto@gmail.com>
2024-11-21 12:31:47 -0300

    :wrench: feat: Criação de rotas protegidas e ajuste da home

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import { useAuth } from 'vue-clerk'
 import Home from '../views/Home.vue'
 import Career from '../views/CareerSelection.vue'
 import Subjects from '../views/Subjects.vue'
@@ -12,24 +12,28 @@ const routes = [
         component: Home,
     },
     {
-        path: '/carreira', 
+        path: '/carreiras', 
         name: 'Carreiras',
         component: Career,
+        meta: { requiresAuth: true },
     },
     {
         path: '/materias', 
         name: 'Materias',
         component: Subjects,
+        meta: { requiresAuth: true },
     },
     {
         path: '/ciclo-de-estudos',
         name: 'Ciclo', 
         component: StudyCycle,
+        meta: { requiresAuth: true },
     },
     {
         path: '/estudar', 
         name: 'Estudar',
         component: StudyStart,
+        meta: { requiresAuth: true },
     }
     
 ]
@@ -37,6 +41,17 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
   routes,
-})
+});
+
+router.beforeEach((to, from, next) => {
+    const { isSignedIn } = useAuth();
+
+    //Se a rota exigir autenticação e o usuário não estiver logado, redirecione para a home
+    if(to.meta.requiresAuth && !isSignedIn.value) {
+        next({ path: '/'});
+    } else {
+        next();
+    }
+});
 
 export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,28 @@
 <script setup>
-import { SignedIn, SignedOut, SignInButton, UserButton } from 'vue-clerk'
-import Button from '../components/ui/Button.vue'
+import { SignInButton, useAuth } from 'vue-clerk';
+import Button from '../components/ui/Button.vue';
+import { useRouter } from 'vue-router';
+import { onMounted, watchEffect } from 'vue';
+
+const { isSignedIn } = useAuth();
+const router = useRouter();
+
+onMounted(() => {
+  if(isSignedIn.value) {
+    router.push({name:'Carreiras'});
+  }
+});
+
+watchEffect(() => {
+  if (isSignedIn.value) {
+    router.push({ name: 'Carreiras' });
+  }
+});
+
 </script>
 
 <template>
-  <div class="grid h-full grid-cols-2">
+  <div class="grid h-screen grid-cols-2">
     <!-- Esquerda -->
      <div class="flex flex-col h-full justify-center p-8 max-w-[550px] mx-auto">
       <h1 class="text-4xl font-bold mb-3">Bem-vindo</h1>
@@ -14,13 +32,11 @@ import Button from '../components/ui/Button.vue'
        </SignInButton>
      </div>
      <!-- Direita -->
-      <div class="relative h-full w-full">
-        <p>Espaço para colocar uma imagem</p>
+      <div class="relative w-full">
+        <!-- <p>Espaço para colocar uma imagem</p> -->
+        <div class="w-full h-full">
+          <img src="../../public/img/wallpaper.jpeg" alt="Wallpaper" class="h-full object-cover">
+        </div>
       </div>
-         <SignedOut>
-         </SignedOut>
-         <SignedIn>
-           <UserButton />
-         </SignedIn>
   </div>
 </template> 
