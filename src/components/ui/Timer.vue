680b8712cef0d33adf8a7e0ebf77f48dbcae2a42
Ramon Noleto <devnoleto@gmail.com>
2024-12-10 16:35:59 -0300

    :wrench: feat: Ajuste de designer do Timer

--- src/components/ui/Timer.vue
+++ src/components/ui/Timer.vue
@@ -21,22 +21,21 @@ const stopTimer = () => {
 </script>
 
 <template>
-  <div class="grid grid-cols-2 rounded-md bg-white p-4 shadow">
+  <div class="flex flex-col rounded-md bg-white p-4 shadow">
     <div class="flex flex-col items-center">
-      <h2 class="text-lg font-bold mb-4">Temporizador</h2>
-      <div class="text-2xl font-mono mb-4">{{ timerStore.formattedTime }}</div>
+      <h2 class="w-full text-2xl text-center p-2 mb-4 border-b border-zinc-100">Temporizador</h2>
+      <div class="text-4xl font-mono mb-4">{{ timerStore.formattedTime }}</div>
       <div class="flex space-x-2">
         <Button @click="timerStore.start" variant="primary" :disabled="timerStore.isRunning || props.isDisabled">Iniciar</Button>
         <Button @click="timerStore.togglePause" variant="secondary" :disabled="!timerStore.isRunning">
           {{ timerStore.isPaused ? 'Continuar' : 'Pausar' }}
         </Button>
-        <!-- O botão "Parar" só estará habilitado quando o timer estiver rodando ou pausado -->
         <Button @click="stopTimer" variant="delete" :disabled="!timerStore.isRunning">Parar</Button>
       </div>
     </div>
-    <div class="flex flex-col text-center justify-center p-4 gap-2 border-l border-zinc-100">
-      <strong>Total de pausas: {{ timerStore.pauses.length }}</strong>      
-      <strong>Tempo total: {{ timerStore.formatPauseTime(timerStore.totalPauseTime) }}</strong>
+    <div class="flex justify-between mt-4 px-4">
+      <p><strong>Tempo total:</strong> {{ timerStore.formatPauseTime(timerStore.totalPauseTime) }}</p>
+      <p><strong>Total de pausas:</strong> {{ timerStore.pauses.length }}</p>      
     </div>
   </div>
 </template>
