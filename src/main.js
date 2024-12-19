8e6ec3ee2f93b2be1cdb2c8ed2295c5b73238e9f
Ramon Noleto <ramon.zap@hotmail.com>
2024-12-18 23:48:45 -0300

    :wrench: bkp: Ainda não está conectando ao endpoint da vercel

--- src/main.js
+++ src/main.js
@@ -15,7 +15,9 @@ import router from './router'
 
 const pinia = createPinia();
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+
+axios.defaults.baseURL = 'https://gerenciamento-de-estudo-api.vercel.app/api/api';
 
 const app = createApp(App);
 
