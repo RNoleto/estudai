af4dc657680677be0b8511f339dc351975f14e27
Ramon Noleto <ramon.zap@hotmail.com>
2025-01-19 17:00:53 -0300

    Ajuste de menu mobile para dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,16 @@
 <script setup>
 import { RouterView, useRoute } from 'vue-router';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import { useUserStore } from '../stores/useUserStore';
 
 const route = useRoute();
+
+const isMobileView = computed(() => window.innerWidth < 400);
+const isMenuMobileOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuMobileOpen.value = !isMenuMobileOpen.value
+}
 
 const userStore = useUserStore();
 
@@ -13,6 +20,7 @@ const isSidebarCollapsed = ref(false);
 // Estados para controlar a abertura dos menus colapsáveis
 const isMenu1Open = ref(false);
 const isMenu2Open = ref(false);
+
 const isMenu3Open = ref(false);
 
 // Funções para alternar os estados dos menus
@@ -30,58 +38,83 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="flex bg-gray-100">
-      <!-- Sidebar -->
-      <aside :class="[
-        'min-h-screen bg-white shadow-md transition-all duration-300',
-        isSidebarCollapsed ? 'w-18' : 'w-60'
-      ]">
-        <div class="p-4 text-lg font-bold border-b flex justify-between items-center">
-          <span v-if="!isSidebarCollapsed">Área do Aluno</span>
-          <button @click="toggleSidebar" class="p-2 text-gray-700 hover:bg-gray-200 rounded">
-            <i :class="isSidebarCollapsed ? 'fas fa-angle-right' : 'fas fa-angle-left'"></i>
-          </button>
-        </div>
-        <nav class="flex flex-col gap-1 " :class="isSidebarCollapsed ? 'p-1' : 'p-2'">
-          <!-- Home da Dashboard -->
-          <div>
-            <a
-            href="/area-do-aluno"
-            :class="[
-              'flex items-center gap-2 px-4 py-2 mt-4 rounded',
-              route.path === '/area-do-aluno' ? 'bg-blue-100 text-blue-700' : 'text-gray-700 hover:bg-gray-100'
-            ]"
-          >
+  <div class="grid bg-gray-100 sm:flex">
+    <!-- sidebar mobile -->
+    <nav v-if="isMobileView"
+      class="fixed top-0 left-0 w-full bg-white shadow-md z-50 flex items-center justify-between p-4">
+      <p class="font-bold text-lg">Área do Aluno</p>
+      <button @click="toggleMenu" class="text-xl p-2 rounded focus:outline-none">
+        <i :class="isMenuMobileOpen ? 'fas fa-times' : 'fas fa-bars'"></i>
+      </button>
+    </nav>
+    <div v-if="isMenuMobileOpen"
+      class="fixed top-0 left-0 w-full h-screen bg-white shadow-lg z-40 flex flex-col items-center justify-start pt-20 space-y-4">
+      <a href="/area-do-aluno" class="block text-gray-700 text-lg font-semibold hover:text-blue-600">
+        Home
+      </a>
+      <a
+        href="/area-do-aluno/historico-de-estudos"
+        class="block text-gray-700 text-lg font-semibold hover:text-blue-600"
+      >
+        Histórico de Estudo
+      </a>
+      <a
+        href="/area-do-aluno/estudar"
+        class="block text-gray-700 text-lg font-semibold hover:text-blue-600"
+      >
+        Estudar
+      </a>
+      <a
+        href="/area-do-aluno/configuracoes"
+        class="block text-gray-700 text-lg font-semibold hover:text-blue-600"
+      >
+        Configurações
+      </a>
+    </div>
+    <!-- Sidebar desktop -->
+    <aside v-if="!isMobileView" :class="[
+      'min-h-screen bg-white shadow-md transition-all duration-300',
+      isSidebarCollapsed ? 'w-18' : 'w-60'
+    ]">
+      <div class="p-4 text-lg font-bold border-b flex justify-between items-center">
+        <span v-if="!isSidebarCollapsed">Área do Aluno</span>
+        <button @click="toggleSidebar" class="p-2 text-gray-700 hover:bg-gray-200 rounded">
+          <i :class="isSidebarCollapsed ? 'fas fa-angle-right' : 'fas fa-angle-left'"></i>
+        </button>
+      </div>
+      <nav class="flex flex-col gap-1 " :class="isSidebarCollapsed ? 'p-1' : 'p-2'">
+        <!-- Home da Dashboard -->
+        <div>
+          <a href="/area-do-aluno" :class="[
+            'flex items-center gap-2 px-4 py-2 mt-4 rounded',
+            route.path === '/area-do-aluno' ? 'bg-blue-100 text-blue-700' : 'text-gray-700 hover:bg-gray-100'
+          ]">
             <i class="fas fa-home"></i>
             <span v-if="!isSidebarCollapsed">Home</span>
           </a>
-          </div>
-          <!-- Historico de Estudos -->
-          <div>
-            <a href="/area-do-aluno/historico-de-estudos" 
-            :class="[
-              'flex items-center gap-2 px-4 py-2 rounded', 
-              route.path === '/area-do-aluno/historico-de-estudos' ? 'bg--blue-100 text-blue-700' : 'text-gray-700 hover:bg-gray-100']"
-            >
-            <i class="fa-solid fa-pen-clip"></i>
-              <span v-if="!isSidebarCollapsed">Histórico de Estudo</span>
-            </a>
-          </div>
-          <!-- Estudar da Dashboard -->
-          <div>
-            <a
-            href="/area-do-aluno/estudar"
+        </div>
+        <!-- Historico de Estudos -->
+        <div>
+          <a href="/area-do-aluno/historico-de-estudos"
             :class="[
               'flex items-center gap-2 px-4 py-2 rounded',
-              route.path === '/area-do-aluno/estudar' ? 'bg-blue-100 text-blue-700' : 'text-gray-700 hover:bg-gray-100'
-            ]"
-          >
+              route.path === '/area-do-aluno/historico-de-estudos' ? 'bg--blue-100 text-blue-700' : 'text-gray-700 hover:bg-gray-100']">
+            <i class="fa-solid fa-pen-clip"></i>
+            <span v-if="!isSidebarCollapsed">Histórico de Estudo</span>
+          </a>
+        </div>
+        <!-- Estudar da Dashboard -->
+        <div>
+          <a href="/area-do-aluno/estudar" :class="[
+            'flex items-center gap-2 px-4 py-2 rounded',
+            route.path === '/area-do-aluno/estudar' ? 'bg-blue-100 text-blue-700' : 'text-gray-700 hover:bg-gray-100'
+          ]">
             <i class="fa-solid fa-stopwatch"></i>
             <span v-if="!isSidebarCollapsed">Estudar</span>
           </a>
-          </div>
-          <!-- Menu Principal 1 -->
-          <!-- <div>
+        </div>
+        <!-- Menu Principal 1 -->
+        <!-- <div>
             <button @click="toggleMenu1"
               class="flex gap-1 items-center justify-between w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100 rounded">
               <div class="flex items-center gap-2">
@@ -96,8 +129,8 @@ onMounted(async () => {
             </div>
           </div> -->
 
-          <!-- Menu Principal 2 -->
-          <!-- <div class="mt-4">
+        <!-- Menu Principal 2 -->
+        <!-- <div class="mt-4">
             <button @click="toggleMenu2"
               class="flex gap-1 items-center justify-between w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100 rounded">
               <div class="flex items-center gap-2">
@@ -111,35 +144,41 @@ onMounted(async () => {
               <a href="#" class="block text-sm text-gray-600 hover:text-gray-900">Submenu 2</a>
             </div>
           </div>   -->
-          <!-- Menu de Configuração -->
-          <div class="">
-            <button @click="toggleMenu3"
-              class="flex gap-1 items-center justify-between w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100 rounded">
-              <div class="flex items-center gap-2">
-                <i class="fas fa-cog"></i>
-                <span v-if="!isSidebarCollapsed">Configurações</span>
-              </div>
-              <i class="fas" :class="[isMenu3Open ? 'fa-chevron-up' : 'fa-chevron-down', isSidebarCollapsed ? 'mini' : 'text-base']"></i>
-            </button>
-            <div v-if="isMenu3Open" :class="['mt-1 space-y-2 transition-all duration-300', isSidebarCollapsed ? 'absolute z-10 rounded-md left-16 bg-zinc-50 shadow-lg w-48 p-2' : 'pl-8']">
-              <a href="/area-do-aluno/carreiras" class="flex gap-2 items-center text-sm text-gray-600 hover:text-gray-900"><i class="fa-solid fa-user-astronaut"></i> Minha Carreira</a>
-              <a href="/area-do-aluno/materias" class="flex gap-2 items-center text-sm text-gray-600 hover:text-gray-900"><i class="fa-solid fa-book"></i> Minhas Matérias</a>
-              <!-- <a href="/area-do-aluno/ciclo-de-estudos" class="flex gap-2 items-center text-sm text-gray-600 hover:text-gray-900"><i class="fa-solid fa-arrows-spin"></i> Meu Ciclo de Estudo</a> -->
-              <!-- <a href="#" class="flex gap-2 items-center text-sm text-gray-600 hover:text-gray-900"><i class="fa-solid fa-crosshairs"></i> Meus Objetivos</a> -->
+        <!-- Menu de Configuração -->
+        <div class="">
+          <button @click="toggleMenu3"
+            class="flex gap-1 items-center justify-between w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100 rounded">
+            <div class="flex items-center gap-2">
+              <i class="fas fa-cog"></i>
+              <span v-if="!isSidebarCollapsed">Configurações</span>
             </div>
+            <i class="fas"
+              :class="[isMenu3Open ? 'fa-chevron-up' : 'fa-chevron-down', isSidebarCollapsed ? 'mini' : 'text-base']"></i>
+          </button>
+          <div v-if="isMenu3Open"
+            :class="['mt-1 space-y-2 transition-all duration-300', isSidebarCollapsed ? 'absolute z-10 rounded-md left-16 bg-zinc-50 shadow-lg w-48 p-2' : 'pl-8']">
+            <a href="/area-do-aluno/carreiras"
+              class="flex gap-2 items-center text-sm text-gray-600 hover:text-gray-900"><i
+                class="fa-solid fa-user-astronaut"></i> Minha Carreira</a>
+            <a href="/area-do-aluno/materias"
+              class="flex gap-2 items-center text-sm text-gray-600 hover:text-gray-900"><i class="fa-solid fa-book"></i>
+              Minhas Matérias</a>
+            <!-- <a href="/area-do-aluno/ciclo-de-estudos" class="flex gap-2 items-center text-sm text-gray-600 hover:text-gray-900"><i class="fa-solid fa-arrows-spin"></i> Meu Ciclo de Estudo</a> -->
+            <!-- <a href="#" class="flex gap-2 items-center text-sm text-gray-600 hover:text-gray-900"><i class="fa-solid fa-crosshairs"></i> Meus Objetivos</a> -->
           </div>
-        </nav>
-      </aside>
-      <!-- Conteúdo Principal -->
-      <main class="flex-1 p-6">
-        <!-- Conteudo a ser carregado na página -->
-        <router-view />
-      </main>
-    </div>
+        </div>
+      </nav>
+    </aside>
+    <!-- Conteúdo Principal -->
+    <main class="flex-1 p-6">
+      <!-- Conteudo a ser carregado na página -->
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <style scoped>
-.mini{
+.mini {
   font-size: 12px;
 }
 </style>
