db4a619c8fac5ba87326557307f4e53861aa80a7
Ramon Noleto <devnoleto@gmail.com>
2025-01-22 16:16:01 -0300

    :iphone: feat: Criação de componente card reutilizavel

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        required: true,
+        default: "Card Title"
+    },
+    content: {
+        type: String,
+        required: true,
+        default: "Card Content"
+    },
+    footer: {
+        type: String,
+        required: true,
+        default: "Card Footer"
+    }
+});
+</script>
+
+<template>
+    <!-- Mobile First -->
+    <div class="border rounded-lg shadow-md border-zinc-200 p-2 bg-white text-xs">
+        <p class="text-sm">{{ title }}</p>
+        <p>{{ content }}</p>
+        <p>{{ footer }}</p>
+    </div>
+</template>
