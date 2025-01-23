8e15ec3e88b76f7f7ed2321f0d452a9a676394c8
Ramon Noleto <devnoleto@gmail.com>
2025-01-23 13:20:53 -0300

    :iphone: teste

--- src/components/ui/ComboBox.vue
+++ src/components/ui/ComboBox.vue
@@ -46,7 +46,7 @@ const handleSelection = (option) => {
 <template>
   <ComboboxRoot v-model="selectedOption" class="relative">
     <ComboboxAnchor
-      class="w-full inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-gray-700 shadow hover:bg-gray-100 outline-none">
+      class="w-full inline-flex items-center justify-between rounded px-[15px] text-[16px] leading-none h-[35px] gap-[5px] bg-white text-gray-700 shadow hover:bg-gray-100 outline-none">
       <ComboboxTrigger class="w-full flex justify-between">
         <ComboboxInput
           class="w-full !bg-transparent outline-none text-gray-700 h-full selection:bg-gray-300"
@@ -64,7 +64,7 @@ const handleSelection = (option) => {
           <ComboboxItem
             v-for="option in options"
             :key="option.id"
-            class=""
+            class="text-base leading-none text-gray-700 rounded flex items-center h-[25px] px-[25px] relative cursor-pointer hover:bg-gray-200"
             :value="option"
             @click="handleSelection(option)">
             <ComboboxItemIndicator
