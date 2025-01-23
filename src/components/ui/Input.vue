bf356fdc503512f4183bf754526e155d33367d79
Ramon Noleto <devnoleto@gmail.com>
2025-01-23 13:39:08 -0300

    :iphone: fix: Correção do tamanho de fonte para o componente de input

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -25,7 +25,7 @@ const emit = defineEmits(['update:modelValue']);
         <label 
         v-if="showLabel"
             :for="id"
-            class="text-sm font-bold text-zinc-800"
+            class="text-base font-bold text-zinc-800"
         >{{ label }}</label>
         <input 
             :id="id" 
@@ -33,7 +33,7 @@ const emit = defineEmits(['update:modelValue']);
             :placeholder="placeholder"
             :value="modelValue"
             @input="$emit('update:modelValue', $event.target.value)"
-            class="w-full inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-gray-700 shadow hover:bg-gray-100 outline-none"
+            class="w-full inline-flex items-center justify-between rounded px-[15px] text-base leading-none h-[35px] gap-[5px] bg-white text-gray-700 shadow hover:bg-gray-100 outline-none"
         />
     </div>
 </template>
