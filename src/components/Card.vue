861fccbc6e75c43ca1fd8303eec0d4ed52b6bf69
Ramon Noleto <ramon.zap@hotmail.com>
2025-01-22 21:40:17 -0300

    :recycle: feat: Melhoria do componente Card e utilização na dashboardHome para campos mobile

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,28 +1,41 @@
 <script setup>
 defineProps({
-    title: {
-        type: String,
-        required: true,
-        default: "Card Title"
-    },
-    content: {
-        type: String,
-        required: true,
-        default: "Card Content"
-    },
-    footer: {
-        type: String,
-        required: true,
-        default: "Card Footer"
-    }
+  title: {
+    type: String,
+    required: true,
+    default: "Card Title",
+  },
+  footer: {
+    type: String,
+    required: false,
+    default: "campo de footer",
+  },
 });
 </script>
 
 <template>
-    <!-- Mobile First -->
-    <div class="border rounded-lg shadow-md border-zinc-200 p-2 bg-white text-xs">
-        <p class="text-sm">{{ title }}</p>
-        <p>{{ content }}</p>
-        <p>{{ footer }}</p>
+  <div class="flex flex-col justify-center bg-white shadow-md rounded-lg text-center px-2 py-4 gap-1 sm:p-4 sm:gap-4 sm:min-h-[250px]">
+    <!-- Título -->
+    <p class="text-sm sm:text-2xl font-bold text-gray-800">{{ title }}</p>
+
+    <!-- Slot para campos opcionais -->
+    <div class="flex fles-col gap-2">
+        <slot name="fields">
+            <!-- Padrão caso não seja fornecido -->
+             <!-- <p class="text-xs text-gray-500">Nenhum campo foi adicionado.</p> -->
+        </slot>
+     </div>
+
+    <!-- Slot para o conteúdo com estilos padrão -->
+    <div class="text-2xl font-bold sm:text-4xl text-blue-600">
+      <slot name="content">Conteúdo padrão</slot>
     </div>
+
+    <!-- Slot para footer com estilos padrão -->
+    <div class="text-xs text-gray-600">
+      <slot name="footer">
+        {{ footer }}
+      </slot>
+    </div>
+  </div>
 </template>
--- src/views/DashboardHome.vue
+++ src/views/DashboardHome.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import StudyTimeRecord from '../layouts/StudyTimeRecord.vue';
 import StudyStatisticsRecords from '../layouts/StudyStatisticsRecords.vue';
 import SubjectSummaryTable from '../layouts/SubjectSummaryTable.vue';
@@ -9,7 +9,42 @@ import Card from '../components/Card.vue';
 import { useUserStore } from '../stores/useUserStore';
 const userStore = useUserStore();
 
+import { useTimeFormatter } from '../composables/useTimeFormatter';
+const { formatStudyTime } = useTimeFormatter();
+
+const totalStudyTime = computed(() => {
+    return userStore.userStudyRecords.reduce((sum, record) => sum + record.study_time, 0);
+});
+
 const hasStudyRecords = ref(false);
+
+// Opções para o combobox
+const options = [
+    { label: 'Questões resolvidas', value: 'questions_resolved' },
+    { label: 'Questões corretas', value: 'correct_answers' },
+    { label: 'Questões incorretas', value: 'incorrect_answers' },
+];
+
+// Estado para a seleção do combobox
+const selectedOption = ref(options[0].value);
+
+// Estado para o tipo de exibição (contador ou porcentagem)
+const displayAsPercentage = ref(false);
+
+// Soma o total com base na seleção
+const totalValue = computed(() => {
+    const totalQuestions = userStore.userStudyRecords.reduce((sum, record) => sum + record.questions_resolved, 0);
+    const selectedTotal = userStore.userStudyRecords.reduce(
+        (sum, record) => sum + record[selectedOption.value],
+        0
+    );
+
+    if (displayAsPercentage.value && totalQuestions > 0) {
+        return ((selectedTotal / totalQuestions) * 100).toFixed(2) + '%';
+    }
+
+    return selectedTotal;
+});
 
 onMounted(async () => {
     try {
@@ -20,14 +55,13 @@ onMounted(async () => {
         console.error(error);
     }
 })
-
 </script>
 
 <template>
     <div class="mt-8 flex flex-col gap-4">
         <div class="flex flex-col gap-2">
             <h3 class="text-xl font-bold sm:text-4xl">Resumo dos estudos</h3>
-            <p>Carreira: {{ userStore.careerName ? userStore.careerName : "Carregando..."  }}</p>
+            <p>Carreira: {{ userStore.careerName ? userStore.careerName : "Carregando..." }}</p>
         </div>
         <div v-if="!hasStudyRecords" class="text-center text-gray-700">
             <p>Você não tem dados de estudos no momento!</p>
@@ -35,15 +69,57 @@ onMounted(async () => {
         <div v-else>
             <div class="hidden sm:flex sm:gap-2">
                 <StudyTimeRecord />
-                <StudyStatisticsRecords  />
-                <SubjectSummaryTable/>
+                <StudyStatisticsRecords />
+                <SubjectSummaryTable />
             </div>
             <div class="sm:hidden">
                 <div class="grid grid-cols-2 gap-2 h-min-[500px]">
-                    <StudyTimeRecord />
-                    <StudyStatisticsRecords />
+                    <!-- Card de tempo de estudo -->
+                    <Card title="Tempo de estudo">
+                        <template #content>
+                            <p>{{ formatStudyTime(totalStudyTime) }}</p>
+                        </template>
+                        <template #footer>
+                            <p>Acompanhe o seu progresso!</p>
+                        </template>
+                    </Card>
+                    <!-- Card de restões respondidas -->
+                    <Card title="Questões respondidas">
+                        <!-- Slote content -->
+                        <template #content>
+                            <p :class="{
+                                'text-blue-600': selectedOption === 'questions_resolved',
+                                'text-green-600': selectedOption === 'correct_answers',
+                                'text-red-600': selectedOption === 'incorrect_answers'
+                            }">
+                                {{ totalValue }}
+                            </p>
+                        </template>
+                        <!-- Slot fields -->
+                        <template #fields>
+                            <!-- Select para opções -->
+                            <select v-model="selectedOption"
+                                class="w-full text-xs p-1 border border-gray-200 rounded-md focus:ring focus:ring-blue-300">
+                                <option v-for="option in options" :key="option.value" :value="option.value">
+                                    {{ option.label }}
+                                </option>
+                            </select>
+                        </template>
+                        <!-- Checkbox para porcentagem -->
+                        <div class="flex items-center py-2 gap-1">
+                            <input type="checkbox" v-model="displayAsPercentage" class="form-checkbox text-blue-600" />
+                            <label class="text-xs text-gray-600">Exibir como porcentagem</label>
+                        </div>
+                        <template #footer>
+                            <!-- Checkbox para porcentagem -->
+                            <div class="flex items-center justify-center gap-1">
+                                <input type="checkbox" v-model="displayAsPercentage"
+                                    class="form-checkbox text-blue-600" />
+                                <label class="text-xs text-gray-600">Exibir como porcentagem</label>
+                            </div>
+                        </template>
+                    </Card>
                     <SubjectSummaryTable/>
-                    <!-- <Card title="Carreira" :content="userStore.careerName ? userStore.careerName : 'Carregando..'"/> -->
                 </div>
             </div>
         </div>
