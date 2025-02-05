dc50c05aa1c21c8b819e6710a67bdbc7b719ed01
Ramon Noleto <devnoleto@gmail.com>
2025-02-05 11:01:00 -0300

    :recycle: feat: Retirada de webkit-appearence para campos de numeros

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -37,3 +37,15 @@ const emit = defineEmits(['update:modelValue']);
         />
     </div>
 </template>
+
+<style scoped>
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+
+input[type="number"] {
+    -moz-appearance: textfield;
+}
+</style>
