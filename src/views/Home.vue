91db86fb39a22036d2a459876c64a583572704d6
Ramon Noleto <ramon.zap@hotmail.com>
2025-02-09 12:27:13 -0300

    :tada: teste home mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,13 @@ import Button from '../components/ui/Button.vue';
 
 <template>
   <!-- Aqui definimos a opacidade do background para 100% (padrão) -->
-  <DefaultLayout backgroundOpacity="opacity-100">
+  <DefaultLayout backgroundOpacity="opacity-100" class="p-2 sm:p-0">
     <!-- Seu card posicionado com o conteúdo -->
-    <div class="absolute top-[200px] left-[200px] bg-white border rounded-[18px] shadow-md p-[30px] w-[460px]">
-      <h1 class="text-4xl font-bold mb-3 w-[250px] text-[#21BFCA]">
+    <div class="absolute bg-white p-4 border rounded-[18px] shadow-md top-[200px] sm:top-[200px] sm:left-[200px] sm:p-[30px] sm:w-[460px]">
+      <h1 class="font-bold mb-3 text-2xl sm:text-4xl w-[250px] text-[#21BFCA]">
         Bem-vindo ao Estudaí.
       </h1>
-      <p class="text-base mb-[40px] text-zinc-600">
+      <p class="text-sm mb-[40px] text-zinc-600 sm:text-base">
         Seu gerenciador de estudos para organizar seu tempo, acompanhar seu desempenho e alcançar seus objetivos. 
         Simplifique seus estudos e estude com mais eficiência!
       </p>
