cc10887b2a340d15c1f6f03eb46d0614cbff3d3f
Ramon Noleto <devnoleto@gmail.com>
2025-03-20 20:49:08 -0300

    Vercel - Desinstalação do vite-plugin-prerender

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vitePrerender from 'vite-plugin-prerender';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vitePrerender({
-      routes: [
-        '/',
-        '/carreiras',
-        '/materias',
-        '/area-do-aluno',
-        '/area-do-aluno/estudar',
-        '/area-do-aluno/historico-de-estudos',
-        '/area-do-aluno/carreiras',
-        '/area-do-aluno/materias'
-      ]
-    })
-  ],
-  optimizeDeps: {
-    include: ['vite-plugin-prerender']
-  }
+  plugins: [vue()],
 });
