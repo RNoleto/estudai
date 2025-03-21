05ff6a7ef771f1ed00809734b3f76af54c21627e
Ramon Noleto <devnoleto@gmail.com>
2025-03-20 21:47:48 -0300

    Google Search Console - Teste de useHead para indexação de página em StudyHistory e componente History

--- src/layouts/History.vue
+++ src/layouts/History.vue
@@ -175,6 +175,23 @@ const totalAccuracyBgClass = computed(() => {
       ? 'text-baseYellow border-baseYellow'
       : 'text-baseRed border-baseRed';
 });
+
+import { useHead } from '@vueuse/head';
+
+useHead({
+    title: "Estuday | Histórico de Estudos - Acompanhe Seu Progresso e Melhore com IA",
+    meta:[
+    { name: 'description', content: "Veja todo o seu histórico de estudo em um só lugar! Filtre matérias, analise seu desempenho e receba insights com IA para melhorar sua aprendizagem. Estude de forma inteligente com o Estuday!" },
+    { property: 'og:title', content: "Estuday | Histórico de Estudos - Acompanhe Seu Progresso e Melhore com IA" },
+    { property: 'og:description', content: "Veja todo o seu histórico de estudo em um só lugar! Filtre matérias, analise seu desempenho e receba insights com IA para melhorar sua aprendizagem. Estude de forma inteligente com o Estuday!" },
+    { property: 'og:image', content: "https://estuday.com.br/img/wallpaper.png" },
+    { property: 'og:url', content: "https://estuday.com.br/area-do-aluno/historico-de-estudos" },
+    { name: 'twitter:title', content: "Estuday | Histórico de Estudos - Acompanhe Seu Progresso e Melhore com IA" },
+    { name: 'twitter:description', content: "Veja todo o seu histórico de estudo em um só lugar! Filtre matérias, analise seu desempenho e receba insights com IA para melhorar sua aprendizagem. Estude de forma inteligente com o Estuday!" },
+    { name: 'twitter:image', content: "https://estuday.com.br/img/wallpaper.png" },
+    { name: 'keywords', content: "concursos públicos, gestão do tempo de estudo, organização de estudos, preparação para concurso, questões de concurso, acompanhamento de desempenho, planejamento de estudos" }
+    ]
+})
 </script>
 
 <template>
